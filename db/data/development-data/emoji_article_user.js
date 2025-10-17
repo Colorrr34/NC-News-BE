98fd33dc1d68b18fe3279fd7ead7e417ad7eac20
Ricky <tcchan71@gmail.com>
2025-10-17 16:42:54 +0100

    added dev data

---
+++ db/data/development-data/emoji_article_user.js
@@ -0,0 +1,9 @@
+module.exports = [
+  { article_id: 30, emoji_id: 1, username: "happyamy2016" },
+  { article_id: 20, emoji_id: 3, username: "cooljmessy" },
+  { article_id: 25, emoji_id: 7, username: "jessjelly" },
+  { article_id: 17, emoji_id: 2, username: "grumpy19" },
+  { article_id: 8, emoji_id: 8, username: "cooljmessy" },
+  { article_id: 25, emoji_id: 6, username: "tickle122" },
+  { article_id: 20, emoji_id: 5, username: "weegembump" },
+];

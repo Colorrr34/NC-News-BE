7c73df68c88392676905be4e26a81971588c1969
Ricky <tcchan71@gmail.com>
2025-12-09 10:34:52 +0000

    static asset

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(cors());
 
 app.use(express.json());
 
-app.use("/api", express.static("./src/public"));
+app.use("/api", express.static("public"));
 
 app.get("/", (req, res) => {
   res.redirect("/api");
